6c29a49c6e391516a19df2b61dbec559cee6b5ef
nheider <nico.heider96@gmail.com>
2024-08-28 14:27:33 +0200

    .

--- RL_CPP/envs/FieldEnv_v1.py
+++ RL_CPP/envs/FieldEnv_v1.py
@@ -17,14 +17,6 @@ class FieldEnv(gym.Env):
         self.vehicle_width = vehicle_width 
         self.sub_steps = sub_steps         
         self.num_lidar_rays = num_lidar_rays
-        self.path = []
-        self.sg_path_poly = None 
-        self.sg_cover_poly = None 
-        self.sg_field_poly = None 
-
-        self.left_edge = []
-        self.right_edge = []
-        self.path_polygon = []
 
         # Normalized action space: segment distance and curvature 
         self.action_space = spaces.Box(
@@ -46,23 +38,21 @@ class FieldEnv(gym.Env):
         points = np.random.randint(0, self.max_size, size=(self.num_points, 2))
         hull = ConvexHull(points)
         field_polygon = points[hull.vertices].tolist()
-        field_polygon.append(field_polygon[0]) # Ensures Polygon is closed
-        self.field_polygon = field_polygon
         self.sg_field_poly = sg.Polygon(field_polygon)
 
     def random_point_on_polygon_perimeter(self): # To determin the start point and heading of the agent 
         perimeter = 0
-        for i in range(len(self.field_polygon)):
-            x1, y1 = self.field_polygon[i]
-            x2, y2 = self.field_polygon[(i + 1) % len(self.field_polygon)]
+        for i in range(len(self.sg_field_poly.coords)):
+            x1, y1 = self.sg_field_poly.coords[i]
+            x2, y2 = self.sg_field_poly.coords[(i + 1) % len(self.sg_field_poly.coords)]
             perimeter += np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
         random_distance = np.random.uniform(0, perimeter)
         cumulative_distance = 0
 
-        for i in range(len(self.field_polygon)-1):
-            x1, y1 = self.field_polygon[i]
-            x2, y2 = self.field_polygon[(i + 1) % len(self.field_polygon)]
+        for i in range(len(self.sg_field_poly.coords)-1):
+            x1, y1 = self.sg_field_poly.coords[i]
+            x2, y2 = self.sg_field_poly.coords[(i + 1) % len(self.sg_field_poly.coords)]
             segment_length = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
         
             if cumulative_distance + segment_length >= random_distance:
@@ -130,18 +120,18 @@ class FieldEnv(gym.Env):
         segment_angle = steering_angle / self.sub_steps
 
         # new_path and new_polygon get used to update the cell visit count
-        new_polygon = []
-        new_left_edge = []
-        new_right_edge = []
+
+        left_edge = []
+        right_edge = []
 
         for i in range(self.sub_steps):
             if i == 0:
                 mid, top, bot, initial_top, initial_bot = self.next_point_in_path(segment_len, segment_angle, start=True)
-                self.left_edge.append(initial_top)
-                self.right_edge.append(initial_bot)
+                left_edge.append(initial_top)
+                right_edge.append(initial_bot)
 
-                new_left_edge.append(initial_top)
-                new_right_edge.append(initial_bot)
+                left_edge.append(initial_top)
+                right_edge.append(initial_bot)
 
             else:
                 mid, top, bot, _, _ = self.next_point_in_path(segment_len, segment_angle)
@@ -149,37 +139,29 @@ class FieldEnv(gym.Env):
             self.heading = (self.heading + segment_angle) % 360
 
             self.path.extend([mid])
-            self.left_edge.extend([top])
-            self.right_edge.extend([bot])
-
-            new_left_edge.extend([top])
-            new_right_edge.extend([bot])
+            left_edge.extend([top])
+            right_edge.extend([bot])
 
         # Create the polygons: cover_polygon is the newly covered area, path polygon is the overall covered area 
-        self.cover_polygon = new_left_edge + list(reversed(new_right_edge))
-        if self.cover_polygon[0] != self.cover_polygon[-1]:
-            self.cover_polygon.append(self.cover_polygon[0])
-        self.sg_cover_poly = sg.Polygon(self.cover_polygon)
+        cover_polygon = left_edge + list(reversed(right_edge))
+        self.sg_cover_poly = sg.Polygon(cover_polygon)
 
-        if len(self.path_polygon == 0): 
-            self.path_polygon = self.left_edge + list(reversed(self.right_edge))
-            if self.path_polygon[0] != self.path_polygon[-1]:
-                self.path_polygon.append(self.path_polygon[0])
-            sg_path_poly = sg.Polygon(self.path_polygon)
+        if self.sg_path_poly == None: 
+            self.sg_path_poly = self.sg_cover_poly
         else: 
             self.sg_path_poly = boolean_set.join(self.sg_path_poly, self.sg_cover_poly)
 
     def extend_path(self, distance, steering_angle):
         self.steering_to_curve(distance=distance, steering_angle=steering_angle)
     
-    def calc_intersect_area(self, field_poly, path_poly):
-        pg1 = sg.Polygon(field_poly)
-        pg2 = sg.Polygon(path_poly)
-        intersect_area = boolean_set.intersect(pg1, pg2).area()
+    def calc_intersect_area(self, poly_1, poly_2):
+        draw(poly_1)
+        draw(poly_2)
+        intersect_area = abs(float(boolean_set.intersect(poly_1, poly_2).area()))
         return intersect_area
     
     def check_if_completed(self):
-        self.completed = (self.field_area == self.coverage_area)
+        return (abs(float(self.sg_field_poly.area())) == self.calc_intersect_area(self.sg_field_poly, self.sg_path_poly))
 
     def calc_path_intersection(): 
         # to do 
@@ -199,9 +181,9 @@ class FieldEnv(gym.Env):
             # Check for intersection with polygon edges
             min_distance = 1000  # Initialize with max distance
 
-            for j in range(len(self.field_polygon)):
-                p1 = np.array(self.field_polygon[j])
-                p2 = np.array(self.field_polygon[(j + 1) % len(self.field_polygon)])
+            for j in range(len(self.sg_field_poly.coords)):
+                p1 = np.array(self.sg_field_poly.coords[j])
+                p2 = np.array(self.sg_field_poly.coords[(j + 1) % len(self.sg_field_poly.coords)])
                 
                 # Calculate intersection using line segments
                 intersection_point = self._line_intersection(current_position, ray_end, p1, p2)
@@ -234,25 +216,9 @@ class FieldEnv(gym.Env):
         return None     
 
     def check_if_inside(self):
-        point = self.path[-1]
-        polygon = self.field_polygon
-
-        x, y = point
-        n = len(polygon)
-        inside = False
-        
-        p1x, p1y = polygon[0]
-        for i in range(n + 1):
-            p2x, p2y = polygon[i % n]
-            if y > min(p1y, p2y):
-                if y <= max(p1y, p2y):
-                    if x <= max(p1x, p2x):
-                        if p1y != p2y:
-                            xinters = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
-                        if p1x == p2x or x <= xinters:
-                            inside = not inside
-            p1x, p1y = p2x, p2y
-
+        inside = False 
+        print(self.sg_field_poly.oriented_side(sg.Point2(self.path[-1])))  
+        #    inside = True
         return inside
 
 
@@ -262,11 +228,8 @@ class FieldEnv(gym.Env):
         point, heading = self.random_point_on_polygon_perimeter()
         self.heading = heading 
         self.path = [point]
-
         self.sg_path_poly = None 
         self.sg_cover_poly = None 
-        self.path_polygon = []
-
 
 
     def reset(self, seed=None, options=None):
@@ -299,7 +262,7 @@ class FieldEnv(gym.Env):
         
         reward = (alpha * (abs(float(self.sg_cover_poly.area()))/norm)*10000)/ (distance*0.8) #- (beta * self.overlap_area) 
 
-        if self.completed:
+        if self.check_if_completed():
             reward += delta
 
         if self.check_if_inside() is not True: 
@@ -374,7 +337,6 @@ class FieldEnv(gym.Env):
         print("Outside:", self.outside)
         print("Rewards:", self.reward)
         cv2.imwrite('field_map_with_path.png', img)
-
 
     def close(self):
         cv2.destroyAllWindows()
